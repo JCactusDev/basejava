21eea9bc658e91589433b9f2d20f5e6885190539
JCactus <dev.jcactus@gmail.com>
2024-08-11 21:02:35 +0500

    HW11_LazySingleton

--- src/main/java/com/github/jcactusdev/LazySingleton.java
+++ src/main/java/com/github/jcactusdev/LazySingleton.java
@@ -1,15 +1,12 @@
 package com.github.jcactusdev;
 
 public class LazySingleton {
-    volatile private static LazySingleton INSTANCE;
-
-    private LazySingleton(){}
-
-    public LazySingleton getInstance() {
-        return LazySingletonHolder.INSTANCE;
-    }
 
     private static class LazySingletonHolder{
         private static final LazySingleton INSTANCE = new LazySingleton();
+    }
+
+    public LazySingleton getInstance() {
+        return LazySingletonHolder.INSTANCE;
     }
 }
