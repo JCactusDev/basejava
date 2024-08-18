7b215e70f8772080f8667373d7cc2404dda538ed
JCactus <dev.jcactus@gmail.com>
2024-08-18 19:59:09 +0500

    LazySingleton

--- src/main/java/com/github/jcactusdev/LazySingleton.java
+++ src/main/java/com/github/jcactusdev/LazySingleton.java
@@ -1,6 +1,7 @@
 package com.github.jcactusdev;
 
 public class LazySingleton {
+    private static volatile LazySingleton instance;
 
     private static class LazySingletonHolder {
         private static final LazySingleton INSTANCE = new LazySingleton();
--- src/main/java/com/github/jcactusdev/sql/ExceptionUtil.java
+++ src/main/java/com/github/jcactusdev/sql/ExceptionUtil.java
@@ -7,10 +7,9 @@ import java.sql.SQLException;
 public class ExceptionUtil {
     public static RuntimeException convertException(SQLException e) {
         if (e instanceof PSQLException) {
-            // Объект существует в базе данных
             // http://www.postgresql.org/docs/9.3/static/errcodes-appendix.html
             if (e.getSQLState().equals("23505")) {
-                return new RuntimeException(e);
+                return new RuntimeException("23505 - unique_violation", e);
             }
         }
         return new RuntimeException(e);
